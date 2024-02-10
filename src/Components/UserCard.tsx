62a4a472f046a23d5332f3e4c54597d9215b3d66
Md Rashed Ali <tools.rashed@gmail.com>
2024-02-10 15:27:35 +0600

    add /dynamic user data

--- src/Components/UserCard.tsx
+++ src/Components/UserCard.tsx
@@ -2,40 +2,40 @@ import { IoLocation } from "react-icons/io5";
 import { CgOrganisation } from "react-icons/cg";
 import { MdEmail } from "react-icons/md";
 import { Link } from "react-router-dom";
-const UserCard = () => {
-  const userData = {
-    name: "Rashed",
-    image: "https://robohash.org/Terry.png?set=set5",
-    title: "Web Developer",
-    email: "talk.rashed@gmail.com",
-    companyName: "TechNext Ltd",
-  };
+import { User } from "../Pages/Homepage";
+
+
+interface UserCardProps {
+  user: User;
+}
+
+const UserCard = ({ user }:UserCardProps) => {
   return (
     <div className="bg-white p-5 shadow-lg border border-gray-100 rounded gap-5">
       {/* AVATAR || IMAGE */}
       <div className="flex justify-center items-center">
         <img
-          src={userData.image}
-          alt={userData.name}
+          src={user.image}
+          alt={`${user.firstName} ${user.lastName}`}
           className="w-32 h-32 rounded-full shadow-md"
         />
       </div>
 
       {/* USER DETAILS INFO */}
       <div className="text-center">
-        <Link to={`/user/id`}>
-        <h1 className="text-center text-3xl">{userData.name}</h1></Link>
-        <h3 className="text-base text-gray-400">{userData.title}</h3>
-       
+        <Link to={`/user/${user.id}`}>
+          <h1 className="text-center text-3xl">{`${user.firstName} ${user.lastName}`}</h1>
+        </Link>
+        <h3 className="text-base text-gray-400">{user.company.title}</h3>
       </div>
 
       {/* CARD Footer */}
       <div className="mt-5">
         {/* COMPANY AND OTHER INFO */}
-        <ul> 
-            <li className="flex gap-2 items-center"><CgOrganisation/> <h3>{userData.companyName}</h3></li>
-            <li className="flex gap-2 items-center"><IoLocation/><p>629 Debbie Drive, Nashville</p></li>
-            <li className="flex gap-2 items-center"><MdEmail/><p>atuny0@sohu.com</p></li>
+        <ul>
+          <li className="flex gap-2 items-center"><CgOrganisation /> <h3>{user.company.name}</h3></li>
+          <li className="flex gap-2 items-center"><IoLocation /><p>{`${user.address.address}, ${user.address.city}`}</p></li>
+          <li className="flex gap-2 items-center"><MdEmail /><p>{user.email}</p></li>
         </ul>
       </div>
     </div>
--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -6,14 +6,52 @@ import Modal from "../Components/Modal";
 import SearchBar from "../Components/SearchBar";
 import UserCard from "../Components/UserCard";
 import { useLoaderData } from "react-router-dom";
+export interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  image: string;
+  address: {
+    address: string;
+    city: string;
+    coordinates: {
+      lat: number;
+      lng: number;
+    };
+    postalCode: string;
+    state: string;
+  };
+  company: {
+    address: {
+      address: string;
+      city: string;
+      coordinates: {
+        lat: number;
+        lng: number;
+      };
+      postalCode: string;
+      state: string;
+    };
+    department: string;
+    name: string;
+    title: string;
+  };
+}
 
 const Homepage = () => {
   const [isShowModal, setIsShowModal] = useState(false);
   const [viewBy, setViewBy] = useState("grid");
-const userData = useLoaderData()
-console.log(userData)
+  const loaderData = useLoaderData() as User[];
+  if (!loaderData) {
+    return (
+      <div className="h-screen w-screen flex justify-center items-center">
+        Loading...
+      </div>
+    );
+  }
+
   return (
-    
     <div>
       <SearchBar />
       <div className="p-2 mt-2 bg-gray-100 rounded-t flex justify-between">
@@ -63,12 +101,11 @@ console.log(userData)
       </div>
       {!isShowModal && (
         <div className="grid grid-cols-3 gap-5 bg-gray-50 p-3">
-          {userData.map(user => (
-            <div>
-              <UserCard data={user} />
-               </div>
+          {loaderData.map((user) => (
+            <div key={user.id}>
+              <UserCard user={user} />
+            </div>
           ))}
-          
         </div>
       )}
     </div>
